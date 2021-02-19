8237f72fdd6b302f027cf9dd76d7147e1ad8f04a
VIPIN K P <58673683+VipinVIP@users.noreply.github.com>
2021-02-19 10:49:35 +0530

    Updated name

--- js/greeting.js
+++ js/greeting.js
@@ -3,7 +3,7 @@ const today = new Date();
 const hour = today.getHours();
 
 // Here you can change your name
-const name = ' John Doe';
+const name = ' Vipin ';
 
 // Here you can change your greetings
 const gree1 = 'Go to Sleep!  ';
